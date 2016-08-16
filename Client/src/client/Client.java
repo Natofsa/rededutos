e1370afc6a4fc6a735109e78ce7aad6980fbfbb0
Renato Santos Mascarenhas <natofsa@yahoo.com.br>
2016-08-16 01:56:50 -0300

    inicio

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -3,8 +3,7 @@ package client;
 public class Client {
     
     public static void main(String[] args) {
-       System.out.println("hello world");
-       System.out.println("Vai viado");
+        
     }
     
 }
